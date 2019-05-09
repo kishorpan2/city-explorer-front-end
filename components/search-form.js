08f579392d5f0ed0753361811833da22a328b6a0
Kishor Pandey <kishor.jpandey@gmail.com>
2019-05-09 09:55:37 -0700

    setting

---
+++ components/search-form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Search extends React.Component{
+  render(){
+  return (
+    <React.Fragment>
+      <input></input>
+      <button>click</button>
+    </React.Fragment>
+     
+  );
+};
+}
+export default Search;
